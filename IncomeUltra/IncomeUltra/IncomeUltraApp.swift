cc1c3f5190e98f89f0247b62a250ea4fe13e6aef
Arundas M K <arun@Arundass-Mac-mini.local>
2025-09-27 20:57:05 -0700

    initial project setup

---
+++ IncomeUltra/IncomeUltra/IncomeUltraApp.swift
@@ -0,0 +1,17 @@
+//
+//  IncomeUltraApp.swift
+//  IncomeUltra
+//
+//  Created by Arundas M K on 27/09/25.
+//
+
+import SwiftUI
+
+@main
+struct IncomeUltraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
